f8254ec4cd27a5129049cfc245222b5fc661af67
KeaganGilmore <keagangilmore@gmail.com>
2024-06-27 15:43:06 +0200

    add parrallax scrolling btn stuf about us page

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './Carousel.module.css';
 
 const Carousel = ({ images }) => {
@@ -42,16 +43,29 @@ const Carousel = ({ images }) => {
   return (
     <div className={styles.carouselFrame}>
       <div className={styles.overlay}></div>
-      <div className={styles.carousel}>
-        <button onClick={handlePrevClick} className={styles.arrowButton}>‹</button>
-        <div className={styles.carouselImages}>
-          {images.map((src, index) => (
-            <div key={index} className={`${styles.imageContainer} ${index === currentIndex ? styles.active : ''}`}>
-              <Image src={src} alt={`Image ${index + 1}`} layout="fill" objectFit="contain" />
-            </div>
-          ))}
+      <div className={styles.frameContainer}>
+        <div className={styles.frameOverlay}></div>
+        <div className={styles.frameBackground}></div>
+        <div className={styles.carousel}>
+          <button onClick={handlePrevClick} className={styles.arrowButton}>‹</button>
+          <div className={styles.carouselImages}>
+            {images.map((src, index) => (
+              <div key={index} className={`${styles.imageContainer} ${index === currentIndex ? styles.active : ''}`}>
+                <Image src={src} alt={`Image ${index + 1}`} layout="fill" objectFit="contain" />
+              </div>
+            ))}
+          </div>
+          <button onClick={handleNextClick} className={styles.arrowButton}>›</button>
         </div>
-        <button onClick={handleNextClick} className={styles.arrowButton}>›</button>
+      </div>
+      <div className={styles.sidebar}>
+        <div className={styles.aboutUs}>
+          <h2>About Us</h2>
+          <p>Pa's Place, located in Kelso, Pennington, offers a safe, nurturing environment for youth. Enjoy comfortable dormitories, recreational spaces, and the beautiful Nkomba Beach. We prioritize safety and provide enriching activities for personal growth and community building.</p>
+          <Link href="/aboutUs" legacyBehavior>
+            <a className={styles.learnMoreButton}>Learn More</a>
+          </Link>
+        </div>
       </div>
     </div>
   );
